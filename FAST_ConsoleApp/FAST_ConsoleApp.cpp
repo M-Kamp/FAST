b3e1b75b3e566d4fc4767c5d46b6b773fb56d3e9
Laursen44 <mikkel_laursen88@hotmail.com>
2020-04-10 14:17:12 +0200

    Now this was better - misunderstood algorithm first time with the contiguous part.

--- FAST_ConsoleApp/FAST_ConsoleApp.cpp
+++ FAST_ConsoleApp/FAST_ConsoleApp.cpp
@@ -3,7 +3,6 @@
 #include<iostream>
 #include <opencv2/imgproc/imgproc.hpp>
 
-using namespace std;
 using namespace cv;
 
 class FASTPoint
@@ -53,13 +52,13 @@ int main()
 	Mat greymat;
 	cv::cvtColor(img, greymat, cv::COLOR_BGR2GRAY);
 
-	int N = 12;
+	int N = 15;
 	int padding = 4;
 	double Tthreshold = 0.2;
 
 	auto start = std::chrono::high_resolution_clock::now();
 
-	vector<InterestPoint> interestPoints;
+	std::vector<InterestPoint> InterestPoints;
 	FASTPoint BresenhamCircle[16];
 
 	BresenhamCircle[0] = FASTPoint(0, -3);
@@ -111,61 +110,142 @@ int main()
 
 			if (interestAbove >= 3 || interestBelow >= 3)
 			{
+				
+			}
+			else
+			{
 				continue;
 			}
 #endif
+			uchar UFStreak = 0;
+			uchar UCStreak = 0;
+			uchar UMaxStreak = 0;
+
+			uchar OFStreak = 0;
+			uchar OCStreak = 0;
+			uchar OMaxStreak = 0;
+
+			bool IsRejectedAbove = false;
+			bool IsRejectedUnder = false;
+
 			for (size_t i = 0; i < 16; i++)
 			{
-#ifdef optimised
-				if (i == 0 || i == 4 || i == 8 || i == 12)
-				{
-					continue;
-				}
-#endif
 				int Itest = greymat.at<uchar>(y + BresenhamCircle[i].Y, x + BresenhamCircle[i].X);
 
+				// then we have a valid pixel for the algorithm and need to do the streak logic for above
 				if (Ip + T < Itest)
 				{
-					interestAbove++;
+					if (!IsRejectedAbove)
+					{
+						OFStreak++;
+						if (OFStreak > OMaxStreak)
+							OMaxStreak = OFStreak;
+					}
+					else
+					{
+						OCStreak++;
+						if (OCStreak > OMaxStreak)
+							OMaxStreak = OCStreak;
+					}
+				}
+				else
+				{
+					
+					if (IsRejectedAbove)
+					{
+						if (OFStreak > OMaxStreak)
+							OMaxStreak = OFStreak;
+					}
+					else
+					{
+						if (OCStreak > OMaxStreak)
+							OMaxStreak = OCStreak;
+
+						OCStreak = 0;
+					}
+					
+					IsRejectedAbove = true;
 				}
 
+				// then we have a valid pixel for the algorithm and need to do the streak logic for under
 				if (Ip - T > Itest)
 				{
-					interestBelow++;
+					if (!IsRejectedUnder)
+					{
+						UFStreak++;
+						if (UFStreak > UMaxStreak)
+							UMaxStreak = UFStreak;
+					
+					}
+					else
+					{
+						UCStreak++;
+						if (UCStreak > UMaxStreak)
+							UMaxStreak = UCStreak;
+					}
+				}
+				else
+				{
+					if (IsRejectedUnder)
+					{
+						if (UFStreak > UMaxStreak)
+							UMaxStreak = UFStreak;
+					}
+					else
+					{
+						if (UCStreak > UMaxStreak)
+							UMaxStreak = UCStreak;
+
+						UCStreak = 0;
+					}
+
+					IsRejectedUnder = true;
 				}
 			}
 
-			if (interestAbove >= N || interestBelow >= N)
+			bool IsInterestPixel = false;
+
+			// now see if we have a streak bigger or the same as N
+			if (OMaxStreak >= N || UMaxStreak >= N)
+			{
+				IsInterestPixel = true;
+			}
+			else if (OFStreak + OCStreak >= N || UFStreak + UCStreak >= N)
+			{
+				IsInterestPixel = true;
+			}
+
+			if (IsInterestPixel)
 			{
 				InterestPoint p = InterestPoint(x, y);
-				interestPoints.push_back(p);
+				InterestPoints.push_back(p);
 				// add color to the original image?
-				uchar& color = greymat.at<uchar>(y, x);
+				uchar& Color = greymat.at<uchar>(y, x);
 
-				color = 255;
+				Color = 255;
 
-				img.at<Vec3b>(y, x) = color;
+				img.at<Vec3b>(y, x) = Vec3b(Color, Color, Color);
 			}
 
 		}
 	}
 
 #ifdef optimised
-	cout << "Optimised Version";
-	cout << endl;
+	std::cout << "Optimised Version";
+	std::cout << std::endl;
 #else
-	cout << "Not Optimised Version";
-	cout << endl;
+	std::cout << "Not Optimised Version";
+	std::cout << std::endl;
 #endif
 
 	auto finish = std::chrono::high_resolution_clock::now();
 	std::chrono::duration<double> elapsed = finish - start;
 	std::cout << "Elapsed time: " << elapsed.count() << " s\n";
 
-	cout << "Done";
-	cout << endl;
-	cout << "We found " << interestPoints.size() << " interest points";
-	cout << endl;
+	std::cout << "Done";
+	std::cout << std::endl;
+	std::cout << "We found " << InterestPoints.size() << " interest points";
+	std::cout << std::endl;
 
 	imshow("image", img);
 
